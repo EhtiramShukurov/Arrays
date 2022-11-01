33c44f1d964f3ece096da330bbe777e05f7e3529
EhtiramShukurov <tu74pw0kj@code.edu.az>
2022-11-01 23:44:46 +0400

    Added task scripts

---
+++ Arrays(C-Sharp)/Arrays(C-Sharp)/Program.cs
@@ -0,0 +1,26 @@
+﻿#region Task 1
+//int[] numbers = { 13, 4, 16, 75, 43, 21, 44 };
+//int min = numbers[0];
+//for (int i = 1; i < numbers.Length; i++)
+//{
+//    if (numbers[i] <= min)
+//    {
+//        min = numbers[i];
+//    }
+//}
+//Console.WriteLine(min);
+#endregion
+
+
+#region Task 2
+//int[] numbers = { 1, 2, 3, 6, 43, 24, 5, 7, 39, 21 };
+//int count = 0;
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    if (numbers[i] < 10 && numbers[i] >= 0)
+//    {
+//        count++;
+//    }
+//}
+//Console.WriteLine(count);
+#endregion
